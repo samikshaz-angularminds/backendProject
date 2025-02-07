db971ef56b62ae01ce85b0cb01890a27381bc4ad
samikshaz-angularminds <samiksha@angularminds.in>
2025-02-07 17:12:11 +0530

    incomingRefreshToken is nulled and subscriber model created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,7 +165,7 @@ const logoutUser = asyncHandler(async (req,res)=>{
 const refreshAccessToken = asyncHandler(async(req,res) => {
    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-   if(incomingRefreshToken){
+   if(!incomingRefreshToken){
       throw new ApiError(401,"Unauthorized request");
    }
 
@@ -201,7 +201,9 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
    } catch (error) {
       throw new ApiError(401, error?.message || "Invalid refresh token");
    }
-})
+});
+
+
 
 
 
